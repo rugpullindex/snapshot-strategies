cd12476f0de87849c08b8728b1056863ab13c494
Chaitanya <yourchaitu@gmail.com>
2021-08-11 00:42:30 +0530

    Revert "[uniswap-v3] Add Pilot Protocol (#4)"
    This reverts commit e819e766f5970f095531548328d12f8af3af1c35.

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -123,7 +123,6 @@ import * as tombFinance from './tomb-finance';
 import * as trancheStakingSLICE from './tranche-staking-slice';
 import * as unipoolSameToken from './unipool-same-token';
 import * as unipoolUniv2Lp from './unipool-univ2-lp';
-import * as uniswapV3 from './uniswap-v3';
 import * as l2Deversifi from './l2-deversifi';
 import * as biswap from './biswap';
 
@@ -251,7 +250,6 @@ const strategies = {
   'tranche-staking-slice': trancheStakingSLICE,
   'unipool-same-token': unipoolSameToken,
   'unipool-univ2-lp': unipoolUniv2Lp,
-  'uniswap-v3': uniswapV3,
   'l2-deversifi': l2Deversifi,
   biswap
 };
